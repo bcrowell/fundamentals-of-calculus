27c39a2f923ee35da7906806b558e2f3e4a0b613
Ben Crowell <notreal@fakewillnotwork.invalid>
2014-05-09 19:33:16 -0700

    l'hopital's rule

--- scripts/eruby_util.rb
+++ scripts/eruby_util.rb
@@ -1165,6 +1165,8 @@ def remove_titlecase(title)
   foo.gsub!(/e=mc/,"E=mc") # LM 25
   foo.gsub!(/ke=/,"KE=") # Mechanics 12.4
   foo.gsub!(/k=/,"K=") # Mechanics 12.4; in case I switch from KE to K
+  foo.gsub!(/l'h/,"L'H") # L'Hopital; software isn't smart enough to handle apostrophe and housetop accent
+  foo.gsub!(/L'h/,"L'H")
   # logic above can't handle multi-word patterns
   proper_nouns()["multiword"].each { |proper| # e.g., proper="Big Bang"
     foo.gsub!(/#{Regexp::quote(proper.downcase)}/) {proper} 
