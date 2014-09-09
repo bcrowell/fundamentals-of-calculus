b0290e74721b087e129b588fba8588964d552f34
Ben Crowell <notreal@invalid.com>
2014-09-09 15:37:10 -0700

    alr, ch 2

--- alr/preprocess.rb
+++ alr/preprocess.rb
@@ -8,14 +8,10 @@ Dir.glob('ch*') do |dir|
           print "#{file}\n"
           file =~ /ch\d+\/(.*)\.rbtex/
           f = $1
-          c = "cd #{dir} && m4 -P #{f}.rbtex >#{f}.postm4 && ../../fruby #{f}.postm4 >#{f}.tex && cd -"
+          c = "cd #{dir} && cat ../m4_preamble #{f}.rbtex | m4 -P >#{f}.postm4 && ../../fruby #{f}.postm4 >#{f}.tex && cd -"
           print c+"\n"
-          system(c)
+          system(c) or exit(-1)
         end
     end
   end
 end
-
-#	perl -e 'foreach $$f(<ch*>) {if (-d $$f) {$$f=~/ch(\d\d)/; $$n=$$1; $$c = "cd ch$$n && m4 -P ch$$n.rbtex >ch$${n}.postm4 && cd -"; print "$$c\n"; system $$c}}'
-#	perl -e 'foreach $$f(<ch*>) {if (-d $$f) {$$f=~/ch(\d\d)/; $$n=$$1; $$c = "cd ch$$n && ../../fruby ch$$n.postm4 >ch$${n}temp.tex && cd -"; print "$$c\n"; system $$c}}'
-#	pdflatex alr_fund
